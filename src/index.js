89c99f621ca8b874de1aa67f7459a3db56eadaba
J4F3ET <jslopezz@udistrital.edu.co>
2023-03-22 18:05:27 -0500

    agregando Server

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import path from "path";
+import {fileURLToPath} from "url";
+import {ethernetIP, ethernet2IP} from "./app/admIP.js";
+import readline from "readline";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+//Configucion del servidor
+const SERVER = ethernetIP() || ethernet2IP();
+const PORT = 3000;
+const app = express();
+//Coneccion con el cliente
+
+
+//Estableciendo la carpeta publica para los archivos estaticos(la carpeta de las vistas del cliente)
+
+app.use(
+	"/bootstrap",
+	express.static(path.join(__dirname, "../node_modules/bootstrap/dist"))
+);
+app.use(
+	"/icons",
+	express.static(path.join(__dirname, "../node_modules/bootstrap-icons/font"))
+);
+app.use(express.static(path.join(__dirname + "/public")));
+
+//Configurando el puerto en donde se va ejecutar el servidor
+app.listen(PORT, SERVER, () => {
+	console.log(`Servidor: ${SERVER}:${PORT}`);
+});
---
+++ src/public/app/app.js
@@ -0,0 +1,139 @@
+function updateMap(sede) {
+	const a = document.body.style.backgroundImage;
+	switch (sede) {
+		case 1:
+			GetMap(4.579561842518199, -74.15756434620194, 17,'Facultad Teconológica');
+			document.body.style.backgroundImage =
+				"url('./view/img/tecnologica.png')";
+			break;
+		case 2:
+			GetMap(4.628253905986502, -74.06543145964815, 17,'Facultad de Ingenieria');
+			document.body.style.backgroundImage = "url('./view/img/sede40.png')";
+			break;
+		case 3:
+			GetMap(
+				4.613461006109428,
+				-74.06469022278243,
+				17,
+				"Facultad de Ciencias y Educación"
+			);
+			document.body.style.backgroundImage =
+				"url('./view/img/macarena.png')";
+			break;
+		case 4:
+			GetMap(
+				4.597221679621991,
+				-74.06459963085757,
+				17,
+				"Facultad de Medio Ambiente y Recursos Naturales"
+			);
+			document.body.style.backgroundImage =
+				"url('./view/img/vivero.png')";
+			break;
+		case 5:
+			GetMap(4.638193858844783, -74.18616088852973, 17,'Sede Porvenir');
+			document.body.style.backgroundImage =
+				"url('./view/img/porvenir.png')";
+			break;
+		case 6:
+			GetMap(4.605211532854522, -74.08014224434929, 17,'Facultad de Artes');
+			document.body.style.backgroundImage =
+				"url('./view/img/ASAB.png')";
+			break;
+		case 7:
+			GetMap(4.579561842518199, -74.15756434620194, 17,'Facultad Teconológica Colegios');
+			document.body.style.backgroundImage =
+				"url('./view/img/tecnologica.png')";
+			break;
+		default:
+			GetMap(4.7078228876322425, -74.0727965182916, 11, "Bogotá");	
+	}
+}; 
+function GetMap(cordenada1, cordenada2, acercamiento, titulo) {
+	var loc = new Microsoft.Maps.Location(4.579474508287231, -74.15752865417089);
+	var map = new Microsoft.Maps.Map("#myMap", {
+		credentials:
+			"AtzDh5axnUWYRw6zOgK5nqnd7S2C_f8Yij3gDZiRtTtmvleWFq01t5OX7YogblQi",
+		center: loc,
+		zoom: acercamiento,
+	});
+	if (titulo == "Bogotá") {return;}
+	var pushpin = new Microsoft.Maps.Pushpin(loc, {
+		title: titulo,
+		subTitle: "Bogotá",
+		color: "red",
+	});
+	map.entities.push(pushpin);
+}
+
+function  maps() {
+	var map;
+	map = new Microsoft.Maps.Map("#myMap", {
+		credentials:
+			"AtzDh5axnUWYRw6zOgK5nqnd7S2C_f8Yij3gDZiRtTtmvleWFq01t5OX7YogblQi",
+		center: new Microsoft.Maps.Location(4.579474508287231, -74.15752865417089),
+		zoom: 12,
+	});
+	console.log("hola0");
+	//Load the spatial math module
+	Microsoft.Maps.loadModule("Microsoft.Maps.SpatialMath", async function () {
+		//Create a circle with a given radius around the center location
+		var center = map.getCenter();
+		var radius = 5000; // en metros
+		var vertices = 100; // número de lados del polígono
+		var path = Microsoft.Maps.SpatialMath.getRegularPolygon(
+			center,
+			radius,
+			vertices,
+			Microsoft.Maps.SpatialMath.Meters
+		);
+		var poly = new Microsoft.Maps.Polygon(path, {
+			fillColor: new Microsoft.Maps.Color(100, 0, 102, 204),
+			strokeColor: new Microsoft.Maps.Color(200, 0, 51, 153),
+			strokeThickness: 2,
+		});
+
+		map.entities.push(poly);
+		console.log("hola1");
+		//Add a pushpin at the center location.
+		var pin = new Microsoft.Maps.Pushpin(center);
+		map.entities.push(pin);
+		try {
+			const response = await fetch(
+				"https://datosabiertos.bogota.gov.co/dataset/d451b52f-e30c-43b3-9066-3a7816638fea/resource/4a6462ef-fa2e-4acf-96db-8521c65371e8/download/colegios_2022_09.geojson"
+			);
+			const data = await response.json();
+			const coordinates = [];
+			data.features.forEach((element) =>
+				coordinates.push(element.geometry.coordinates)
+			);
+
+			for (let i = 0; i < coordinates.length; i++) {
+				let latitud = Microsoft.Maps.Location.normalizeLongitude(
+					coordinates[i][1]
+				);
+				let longitud = Microsoft.Maps.Location.normalizeLongitude(
+					coordinates[i][0]
+				);
+				var loc = new Microsoft.Maps.Location(latitud, longitud);
+				var pin = new Microsoft.Maps.Pushpin(loc);
+				map.entities.push(pin);
+			}
+		} catch (err) {
+			console.log(err);
+		}
+
+		console.log("holaFinal");
+		
+	});
+}
+/*
+
+					if (Microsoft.Maps.SpatialMath.containsLocation(loc, poly)) {
+						var pin = new Microsoft.Maps.Pushpin(loc);
+						map.entities.push(pin);
+						console.log('SIN ERROR ',i);
+					}
+*/
+
+
