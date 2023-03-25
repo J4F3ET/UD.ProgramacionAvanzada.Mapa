e70e920b23599ed92b88c7ae129f91e467b8af87
J4F3ET <jslopezz@udistrital.edu.co>
2023-03-25 08:27:05 -0500

    simplificando

--- app.js
+++ app.js
@@ -28,87 +28,66 @@ async function GetMap() {
 }
 const select_sede = document.querySelectorAll(".sede");
 select_sede.forEach((element) => {
-	element.addEventListener("click", () => {
-			clearEntities();
-			updateMap(element.dataset.inputSede);
-	});
+	element.addEventListener("click", () => {updateMap(element.dataset.inputSede);});
 });
-const select_colegios = document.querySelector(".colegios");
-select_colegios.addEventListener("click", () => {
-	const icon = document.getElementById("ojos_icon");
+document.querySelector(".colegios").addEventListener("click", () => {
 	var sede;
-	if(select_colegios.checked){
-		icon.classList.remove("bi-eye-slash");
-		icon.classList.add("bi-eye");
-	}else{
-		icon.classList.remove("bi-eye");
-		icon.classList.add("bi-eye-slash");
-	}
-
-	select_sede.forEach((element) => {
-		if (element.checked) {sede = element.dataset.inputSede;}
-	});
+	icono();
+	select_sede.forEach(element =>
+		element.checked? sede = element.dataset.inputSede:null
+	);
 	if(sede == undefined){return}
-	clearEntities();
-	console.log('sede check ',sede);
 	updateMap(sede);
 });
 function updateMap(eleccion) {
+	map.entities.clear();
 	switch (eleccion) {
 		case ('1'):
-			if (document.querySelector(".colegios").checked){
-				colegios(4.579561842518199, -74.15756434620194);
-			}else{
-				goSede(
-					4.579561842518199,-74.15756434620194,"Facultad Teconológica"
-				);
-			}
+			document.querySelector(".colegios").checked?
+				colegios(4.579561842518199, -74.15756434620194):
+				goSede(4.579561842518199,-74.15756434620194,"Facultad Teconológica");
 			document.body.style.backgroundImage = "url('./img/tecnologica.png')";
 			break;
 		case ('2'):
-			if (document.querySelector(".colegios").checked){
-				colegios(4.628253905986502, -74.06543145964815);
-			}else{
-				goSede(
-					4.628253905986502,-74.06543145964815,"Facultad de Ingenieria"
-				);
-			}
+			document.querySelector(".colegios").checked?
+				colegios(4.628253905986502, -74.06543145964815)
+				: goSede(
+						4.628253905986502,
+						-74.06543145964815,
+						"Facultad de Ingenieria"
+				  );
 			document.body.style.backgroundImage = "url('./img/sede40.png')";
 			break;
 		case ('3'):
-			if (document.querySelector(".colegios").checked){
-				colegios(4.613461006109428,-74.06469022278243);
-			}else{
-				goSede(
-					4.613461006109428,-74.06469022278243,"Facultad de Ciencias y Educación"
-				);
-			}
+			document.querySelector(".colegios").checked? 
+				colegios(4.613461006109428, -74.06469022278243)
+				: goSede(
+						4.613461006109428,
+						-74.06469022278243,
+						"Facultad de Ciencias y Educación"
+				  );
 			document.body.style.backgroundImage = "url('./img/macarena.png')";
 			break;
 		case ('4'):
-			if (document.querySelector(".colegios").checked){
-				colegios(4.597221679621991,-74.06459963085757);
-			}else{
-				goSede(
-					4.597221679621991,-74.06459963085757,"Facultad de Medio Ambiente y Recursos Naturales"
-				);
-			}
+			document.querySelector(".colegios").checked? 
+				colegios(4.597221679621991, -74.06459963085757)
+				:goSede(
+						4.597221679621991,
+						-74.06459963085757,
+						"Facultad de Medio Ambiente y Recursos Naturales"
+				  );
 			document.body.style.backgroundImage = "url('./img/vivero.png')";
 			break;
 		case ('5'):
-			if (document.querySelector(".colegios").checked){
-				colegios(4.638193858844783, -74.18616088852973);
-			}else{
-				goSede(4.638193858844783, -74.18616088852973,"Sede Porvenir");
-			}
+			document.querySelector(".colegios").checked
+				? colegios(4.638193858844783, -74.18616088852973)
+				: goSede(4.638193858844783, -74.18616088852973, "Sede Porvenir");
 			document.body.style.backgroundImage = "url('./img/porvenir.png')";
 			break;
 		case ('6'):
-			if (document.querySelector(".colegios").checked){
-				colegios(4.605211532854522, -74.08014224434929);
-			}else{
-				goSede(4.605211532854522, -74.08014224434929,"Facultad de Artes");
-			}
+			document.querySelector(".colegios").checked
+				? colegios(4.605211532854522, -74.08014224434929)
+				: goSede(4.605211532854522, -74.08014224434929, "Facultad de Artes");
 			document.body.style.backgroundImage = "url('./img/ASAB.png')";
 			break;
 		default:
@@ -130,59 +109,12 @@ function goSede(lat, lon, titulo) {
 	map.entities.push(pushpin);
 }
 function colegios(lat, lon) {
-	var tiempo_inicial = new Date().getTime();
-	Microsoft.Maps.loadModule("Microsoft.Maps.SpatialMath", () => {
-
-		var path = Microsoft.Maps.SpatialMath.getRegularPolygon(
-			new Microsoft.Maps.Location(lat,lon), //centro
-			5, //radio en kilometros
-			100, //numero de lados del poligono
-			Microsoft.Maps.SpatialMath.DistanceUnits.Kilometers
-		);
-
-		var poly = new Microsoft.Maps.Polygon(path, {
-			fillColor: new Microsoft.Maps.Color(100, 0, 102, 204),
-			strokeColor: new Microsoft.Maps.Color(200, 0, 51, 153),
-			strokeThickness: 2,
-		});
-
-		//agrega el poligono al mapa
-		map.entities.push(poly);
-		
+	Microsoft.Maps.loadModule("Microsoft.Maps.SpatialMath",async () => {
+		const poly = await createPolygon(lat, lon);
 		data.features.forEach((element) => {
-			if (Microsoft.Maps.SpatialMath.Geometry.intersects(
-						new Microsoft.Maps.Location(
-							element.geometry.coordinates[1],
-							element.geometry.coordinates[0]
-						), poly
-					)
-				) 
-			{
-				var pin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(
-								element.geometry.coordinates[1],
-								element.geometry.coordinates[0]
-							), 
-							{color: "red"}
-						);
-
-				var infobox = new Microsoft.Maps.Infobox(new Microsoft.Maps.Location(
-							element.geometry.coordinates[1],
-							element.geometry.coordinates[0]
-						),
-						{
-							title: element.properties.NOMBRE_EST,
-							description: element.properties.DIRECCION,
-							visible: false,
-						}
-					);
-
-				infobox.setMap(map);
-
-				Microsoft.Maps.Events.addHandler(pin, "click", function () {
-					infobox.setOptions({visible: true});
-				});
-				map.entities.push(pin);
-
+			var loc = new Microsoft.Maps.Location(element.geometry.coordinates[1],element.geometry.coordinates[0]);
+			if (Microsoft.Maps.SpatialMath.Geometry.intersects(loc,poly)) {
+				createPunshipInfobox(loc,element.properties.NOMBRE,element.properties.DIRECCION);
 			}
 		});
 	});
@@ -190,26 +122,48 @@ function colegios(lat, lon) {
 		new Microsoft.Maps.Location(lat,lon)
 	);
 	map.entities.push(pin);
-
-	map.setView({
-		zoom: 13,
-		center: new Microsoft.Maps.Location(lat,lon),
-		animate: true
-	});
-	var tiempo_final = new Date().getTime();
-	console.log("Tiempo transcurrido: " + (tiempo_final - tiempo_inicial) + "ms");
+	map.setView({zoom: 13,center: new Microsoft.Maps.Location(lat,lon),animate: true});
 }
-function clearEntities() {
-	for (var i = map.entities.getLength() - 1; i >= 0; i--) {
-		var pushpin = map.entities.get(i);
-		if (pushpin instanceof Microsoft.Maps.Pushpin) {
-			map.entities.removeAt(i);
-		}
+function icono(){
+	const icon = document.getElementById("ojos_icon");
+	if (document.querySelector(".colegios").checked) {
+		icon.classList.remove("bi-eye-slash");
+		icon.classList.add("bi-eye");
+	} else {
+		icon.classList.remove("bi-eye");
+		icon.classList.add("bi-eye-slash");
 	}
-	for (var i = map.entities.getLength() - 1; i >= 0; i--) {
-		var polygon = map.entities.get(i);
-		if (polygon instanceof Microsoft.Maps.Polygon) {
-			map.entities.removeAt(i);
-		}
-	}
+}
+function createPolygon(lat, lon) {
+	var promise; 
+	promise = new Promise((resolve)=>{
+		Microsoft.Maps.loadModule("Microsoft.Maps.SpatialMath", () => {
+			var path = Microsoft.Maps.SpatialMath.getRegularPolygon(
+				new Microsoft.Maps.Location(lat, lon), //centro
+				5, //radio en kilometros
+				100, //numero de lados del poligono
+				Microsoft.Maps.SpatialMath.DistanceUnits.Kilometers
+			);
+	
+			var poly = new Microsoft.Maps.Polygon(path, {
+				fillColor: new Microsoft.Maps.Color(100, 0, 102, 204),
+				strokeColor: new Microsoft.Maps.Color(200, 0, 51, 153),
+				strokeThickness: 2,
+			});
+	
+			//agrega el poligono al mapa
+			map.entities.push(poly);
+			resolve(poly);
+		});
+	});
+	return promise;
+}
+function createPunshipInfobox(loc, title, description) {
+	var pin = new Microsoft.Maps.Pushpin(loc,{color: "red",});
+	var infobox = new Microsoft.Maps.Infobox(loc,{title,description,visible: false});
+	infobox.setMap(map);
+	Microsoft.Maps.Events.addHandler(pin, "click", function () {
+		infobox.setOptions({visible: true});
+	});
+	map.entities.push(pin);
 }
